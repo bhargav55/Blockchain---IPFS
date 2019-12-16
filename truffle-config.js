48acce29b237d35bb24fdbb9efad4312098afe51
bhargav55 <kacharlabhargav21@gmail.com>
2019-12-16 09:08:05 -0800

    initial commit

---
+++ truffle-config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  contracts_build_directory: path.join(__dirname, "./client/src/abi"),
+  networks: {
+    development : {
+      host: "127.0.0.1",
+      port: "7545",
+      network_id:"*"
+    }
+   },
+   compilers: {
+    solc: {
+     
+    }
+  }
+};
